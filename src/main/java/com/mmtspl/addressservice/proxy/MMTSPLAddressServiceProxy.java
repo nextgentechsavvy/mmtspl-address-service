ef84acb93e1c9d0d7add58f45362c4bddcfe16ae
mithunmajumdar <mithun.majumdar@gmail.com>
2024-07-17 16:18:32 +0530

    updated

--- src/main/java/com/mmtspl/addressservice/proxy/MMTSPLAddressServiceProxy.java
+++ src/main/java/com/mmtspl/addressservice/proxy/MMTSPLAddressServiceProxy.java
@@ -1,8 +1,8 @@
 package com.mmtspl.addressservice.proxy;
 
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="mmtspl-netflix-zuul-api-gateway-server")
+//@FeignClient(name="mmtspl-netflix-zuul-api-gateway-server")
 public class MMTSPLAddressServiceProxy {
 
 }
